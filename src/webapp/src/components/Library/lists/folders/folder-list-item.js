8dc26ace75c18c89d594d215f44fa2b82b36f5c8
Pavlo Shylan <crabulik@gmail.com>
2026-02-20 11:50:36 +0200

    Fix status view

--- src/webapp/src/components/Library/lists/folders/folder-list-item.js
+++ src/webapp/src/components/Library/lists/folders/folder-list-item.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import {
@@ -9,10 +9,12 @@ import {
 } from '@mui/material';
 
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import EditIcon from '@mui/icons-material/Edit';
 
 import request from '../../../../utils/request';
 import FolderLink from './folder-link';
 import FolderTypeAvatar from './folder-type-avatar';
+import MetadataEditor from '../albums/song-list/MetadataEditor';
 
 const FolderListItem = ({
   folder,
@@ -21,6 +23,7 @@ const FolderListItem = ({
 }) => {
   const { t } = useTranslation();
   const { type, name, relpath } = folder;
+  const [editorOpen, setEditorOpen] = useState(false);
 
   const playItem = () => {
     switch(type) {
@@ -30,7 +33,7 @@ const FolderListItem = ({
       // TODO: Add missing Stream
       default: return;
     }
-  }
+  };
 
   const registerItemToCard = () => {
     switch(type) {
@@ -40,29 +43,61 @@ const FolderListItem = ({
       // TODO: Add missing Stream
       default: return;
     }
-  }
+  };
+
+  // Determine secondary action based on type and mode
+  const getSecondaryAction = () => {
+    if (type === 'directory') {
+      // Directories get the navigate arrow
+      return (
+        <IconButton
+          component={FolderLink}
+          data={{ dir: relpath }}
+          edge="end"
+          aria-label={t('library.folders.show-folder-content')}
+        >
+          <NavigateNextIcon />
+        </IconButton>
+      );
+    } else if (type === 'file' && !isSelecting) {
+      // Files get the edit button (only when not selecting for card registration)
+      return (
+        <IconButton
+          edge="end"
+          size="small"
+          aria-label={t('library.metadata-editor.edit-button-label')}
+          onClick={(e) => {
+            e.stopPropagation();
+            setEditorOpen(true);
+          }}
+        >
+          <EditIcon fontSize="small" />
+        </IconButton>
+      );
+    }
+    return undefined;
+  };
 
   return (
-    <ListItem
-      disablePadding
-      secondaryAction={
-        type === 'directory'
-          ? <IconButton
-              component={FolderLink}
-              data={{ dir: relpath }}
-              edge="end"
-              aria-label={t('library.folders.show-folder-content')}
-            >
-              <NavigateNextIcon />
-            </IconButton>
-          : undefined
-      }
-    >
-      <ListItemButton onClick={() => (isSelecting ? registerItemToCard() : playItem())}>
-        <FolderTypeAvatar type={type} />
-        <ListItemText primary={name} />
-      </ListItemButton>
-    </ListItem>
+    <>
+      <ListItem
+        disablePadding
+        secondaryAction={getSecondaryAction()}
+      >
+        <ListItemButton onClick={() => (isSelecting ? registerItemToCard() : playItem())}>
+          <FolderTypeAvatar type={type} />
+          <ListItemText primary={name} />
+        </ListItemButton>
+      </ListItem>
+
+      {type === 'file' && (
+        <MetadataEditor
+          open={editorOpen}
+          onClose={() => setEditorOpen(false)}
+          song={{ file: relpath, title: name }}
+        />
+      )}
+    </>
   );
 }
 
